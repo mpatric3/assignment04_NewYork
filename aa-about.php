d14ec55ac515dc49b75d07d51e722fac80bbe067
Amber Williams <awilli77@u.rochester.edu>
2018-03-26 09:39:08 -0400

    IA
    Added necessary files from Lagos and Granada Teams and changed the format. Please see readme.md for IA comments. Ignore all internal comments in the htmls and phps as they are from Assignment 3.

---
+++ aa-about.php
@@ -0,0 +1,62 @@
+<?php include "inc/html-top.inc";?>
+    
+    <body>
+        <section class = "banner">
+            <?php include "inc/header.inc"; ?>
+            <?php include "inc/nav.inc"; ?>
+        </section> <!--.banner-->
+
+        <div class = "container">
+        
+        <article>
+            
+             <h2>Alejandro Abreu</h2>
+
+                <div class="info">
+                    <!--no bullet points, just a list next to the picture-->
+                    <ul class="basic-info">
+                        <li>Major: Computer Science</li>
+                        <li>Graduation Year: 2018</li>
+                        <li>Hometown: New York City</li>
+                    </ul>
+                </div>
+
+                <div class="pic">
+                    <!--images of place where each person is from, round-->
+                    <figure class="place"> 
+                        <img class="scale" src="images/aa.jpg" alt="Alejandro Abreu Picture">
+                    </figure>
+                </div>
+            
+            <div class="article-info">
+
+            <!--About and their work experience, two seperate headings-->
+            <h3>About</h3>
+                <p>My name is Alejandro Abreu, and I was born on September 6th, 1995. I was born in Monte Sinai hospital in Manhattan, and a few months later moved to the Bronx where I currently reside. I am from New York City, more specifically, the Bronx. As a youth, I was very active and in love with sports. I spent most of my time at the park or at the gym either playing baseball or basketball. I attended the P.S 220 for elementary school and later moved on the school for Applied Mathematics and Technology, AAMT, for middle school. At AAMT, I played basketball and ran track for the school team. After completing middle school, I moved on and attended high school at A Philip Randolph High School Campus. In high school, I was rejected from joining the varsity baseball team which led me to pursue other sports. This led me to playing Varsity Lacrosse and Bowling. I graduated from A Philip Randolph in Spring 2013. I was accepted to various university but none call out to me like the one that I currently attend. At the moment, I am a senior at the University of Rochester pursuing a degree in Computer Science. I am anticipated to graduate from the University of Rochester in May 2018. In my lifetime I have worked in about 5 different location varying from a gym to a school.</p>
+
+                <p>In addition, I joined the Minority Male Leadership Association, MMLA, during my junior year at the University of Rochester. Joining MMLA gave me the opportunity to provide tutoring and support to male students in Public School 19 in Rochester, NY. The graduation rate in Rochester, NY is considerably low compared to other parts of the United States. Our mission was to show students that we were in the same position as them and we were able to overcome the obstacles that they may be going through themselves. I would go to Public School 19 twice a week, and work a group of three students. I was tasked with sitting in their classes and helping them with their work that they did not understand. I would also answer questions they had about my high school and college experience to the best of my abilities. Till this day, I try to keep in contact with the students that I worked with at Public School 19. I hope to provide the scholars in SEO program with the same support that I was able to provide to Luis Avila, Jordan Fletcher, and Thomas Brown at public school 19.</p>
+
+                <p>While attending the University of Rochester, I have had the opportunity to take a variety of classes due to U of R open curriculum. I think my favorite course that I have taken throughout the years has to be any taught be either Jim Memmott or Curt Smith. At this point I got tired of writing about myself, so these last couple of sentence is a bunch of nonsense. I have a question: A fish underwater… is it wet or dry? I believe it is wet but a lot of people have their own opinions.</p>
+</div>
+</article>
+
+<aside>
+        <div class="aside-info">
+
+            <h3>Work Experience</h3>
+                <p>The first job that I obtained was here at the University of Rochester as a Building Manager at the Goergen Athletic Center. As a building manager at the Goergen Athletic Center, my main responsibilities include ensuring the safety of over 300 students and faculty members using the athletic facility. Building Managers take more than 3 tours a shift around the gym in order to ensure safety and proper usage of our equipment. In addition, a Building Manager is tasked with the responsibility of selling and promoting various membership packages. Finally, we also provide information of building and recreational activities to interested potential and active gym members over the phone or in person.</p>
+
+                <p>I have previously worked with Upward bound as tutor. Upward Bound main focus is to provide high school students with the resources and support they need to get to and through college. As a tutor for Upward Bound, I improved on my communication skills by being responsible for tutoring high school students individually and in small groups who struggled in math. I ensure that every student receives my undivided attention and grasp the material in order to complete their work. When not tasked with tutoring students, I completed an excel spreadsheet on students personal information. Tutoring for Upward Bound has given me some experience in office space environment.</p>
+
+                <p>During the summer, I have been working at the 92Y Camps in New York City as a Camp Counselor. The job was from 8 in the morning till 5 pm in the afternoon. The campers which I was in charge of were heading into fifth or sixth graders. My camp group consisted of 10 to 15 kids throughout the summer. The day would start with counselor heading to their bus site and waiting for their campers to arrive. Once you have gathered all your campers, we would load the buses and head to our camp grounds which is about 30 to 45 minutes away from New York City. Throughout the bus rides, one would entertain the campers by providing them with cheers. As a Camp Counselor, my job was to ensure kids were not only safe but having a fun time at camp. I would participate in all activities that the kids were involved in. They would participate in activities such as basketball, baseball, lacrosse, cooking, and painting. Throughout the week, I would supervise basketball activities as well as baseball. However, occasionally they would switch me depending on how many people we have available. Throughout the summer we would take them on four trips, two overnights and two late trips. During the final week of camp, we take our kids away to sleep away camp for the week where they compete in Color Wars. I had the honor of being the team captain for the Blue team two years in a row. Unfortunately, both the years I served as team captain my team ended up losing.</p>
+        </div> 
+</aside>      
+
+        
+        <?php include "inc/footer.inc";?>
+
+        </div><!--.container -->
+
+        <?php include "inc/scripts.inc";?>
+    </body>
+</html>
---
+++ readme.md
@@ -0,0 +1,17 @@
+Assignment 4: New York
+
+Information Architect: Amber Williams
+	Created HTML template. Chose to use files from Team Granada containing profiles of computer science majors as the three students who are similiar and the profile of Olatunde Oshunlaja from Team Phoenix since his major is Business, different from computer science.
+	Please ignore all internal comments regarding structure that may have been missed. Structure comments applying to page layout and suggestions will be located here.
+	Comments:
+		The home page should have small snippets of each individual. The computer science majors should be groups together with a seperation between the business major to differiniate them. The heading should notate the difference of the majors (i.e. 3 comp sci and a business major). Each profile should contain an identifying picture that is clickable to their expanded profile/respective pages. 
+		Each profile should have only 2 sections (about and work exprience). Intial roles from Granada profiles have been deleted.
+		Article is about each person.
+		Aside is each person's work experience.
+		These sections should correspond with each other in a fluid reading format within each profile page. About should be read first and work experience should be read second. Each page should consist of an identifying photo that is the same deminisions and location for each respective profile page.
+		Font and typeface should be easy to read not crampt. Respective headings should be large, bold, and similiar yet different font.
+		The web form should inculde a space for "name", "email", and a comment section for users to write a simple sentence regarding how computer science and business interconnect in careers. This form should be listed on the home page.
+		The profile page for the business student should differ from the other three pages. Layout should be the same in order to expect similiar flow, but the page should look different (i.e. different color scheme and slightly different font). The headings should also be different. There should be an h1 stating computer science major for the three computer science majors and an h1 stating Business Major for the business major page. These should be centered on the page.
+Design Artist: Ana Vaquera
+	
+Coder: Meghan Patrick
