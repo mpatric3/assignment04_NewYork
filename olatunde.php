1036543ccc4b8b4874fdb7f55a706380c046414b
Amber Williams <awilli77@u.rochester.edu>
2018-03-29 19:50:38 -0400

    Length
    Changed the length of each profile to match each other in size. Edited the business profile to match the format of the 3 other profiles

--- olatunde.php
+++ olatunde.php
@@ -1,5 +1,3 @@
-<!doctype html>
-<html lang = "en">
 <?php include "inc/html-top.inc";?>
 
 <body>
@@ -13,51 +11,61 @@
 
 		<article class = item2>
 		<section class = "hero">
-			<h2> About Me </h2></section>
+			<h2> Olatunde Oshunlaja </h2></section>
 
-			<figure>
+                <div class="info">
+                    <ul class="basic-info">
+                    <!--no bullet points, just a list next to the picture-->
+                    <ul>
+                        <li> Major: Business </li>
+                        <li> Graduation Year: Unknown </li>
+                        <li> Hometown: Unkown </li>
+                    </ul>
+                </div>
+
+		<div class="pic">
+			<figure class="place">
 				<img src="images/oo.jpg" alt="Olatunde Oshunlaja">
-				<figcaption> London Skyline </figcaption>
 			</figure>
+		</div>
 
+		<h3> About </h3>
 			<p>My name is Olatunde Oshunlaja. I know right pretty complex to pronounce so you can just call me "Ola". I am Junior who is majoring in Business and minoring in Computer Science.I Love traveling the world and experiencing new cultures. An interesting tidbit about me is that I have been to school in three different countries Nigeria, England and now the US. I speak two languages English and Yoruba.</p>
 
 			<p> Some of my favorite books include” Extreme ownership” by Jocko Willink, “Tribe” by Sebastian Junger and “The Prince” by Niccolo Machiavelli and the play “Titus Andronicus” by William Shakespeare. My Favorite quote is by the baseball player Mickey Rivers which goes “ain’t no sense worryin’ about things you got control over, ‘cause if you got control over ‘em, aint no sense worryin’. And ain’t no sense worryin’ about the things you don’t got control over, ‘cause if you don’t got control over ‘em, ain’t no sense worryin’. My Philosophy on life is based on Guy Ritchie’s idea of owning the suit. It is an idea that you can construct your own narrative. What kind of messages with your suit do you want to convey to others. My Long term academic goals get my MBA in business administration, get a certification as a CPA or CFA and hopefully work in some capacity as an analyst for the International Monetary Fund. In another life, surely, I would have been a soccer star or Elon Musk.</p>
+		</div>
+	</article>
                 
 
+    <aside>
+    	<div class="aside-info">
 			<section class = "hero">
-			<h2> Work Experience </h2></section>
-			<section class = "hero">
-            <h3> Skye Bank </h3></section>
+			<h3> Work Experience </h3>
+			</section>
 
             <p>Some of my notable work experience includes working for Skye Bank in three 
              departments. The Banking Operations Division Assisted with the development of Marketing Strategies and the Management of Small and Medium Business (SMB)Accounts. Assisted with the Assessment and Collation of Loan Documentation for small businesses. The Customer service division where I was Involved in Client Management which involved activities such as Account Opening, Updated Client Records, Issuance of new Cheque Books, Debit and Credit Cards.</p>
 			
              <section class = "hero">
-			<h3> Quasar Limited </h3></section>
 
 			<p>Another interesting work experience was working with Quasar Limited a Ready-Mix Concrete supplier and I Participated in all aspects of the Plant’s Operations. Assisted with the Collection and Recording of Laboratory Samples, Coordination of Onsite deliveries, Materials Pre-Orders and Creation of a Client Database. Created flow charts for Senior Management’s understanding of the operational bottlenecks of the business.</p>
 			
 			<section class = "hero">
-			<h3> Regional Transit Service </h3></section>
 			
 			<p>Furthermore, I Recently participated in a project in collaboration with the Regional Transit Service who   were redesigning the public transit system in the Monroe County region for the first time in 40 years. I was involved in primary and secondary market research which involved in handing out surveys and organizing focus groups to gather information from the residents of the Monroe county residents to figure out the trends that will encourage increased patronage of the RTS services and also identify the existing problems with RTS services.</p>
 
-
-		</article>
-
-		<aside class =  "item3">
-
-			<section class = "hero">
-			<h2> Extra Curricular Activities </h2></section>
-
-			<p>I am huge fan of Sports. It’s the competition in sports that intrigues me the most.  There is something about watching individuals with some of the greatest talents on the planet dueling against each other that excites me. The sporting activities I have a keen interest include Mixed Martial Arts, Football, Basketball, Formula 1, Track (mostly 100m and 200m) and Soccer. However, Soccer is my one true love. I absolutely love playing and watching soccer. I’m a huge Manchester united Fan. Some of my favorite sportsmen include Lewis Hamilton, Paul Scholes, Rose Namajunas and Khabib Nurmagomedov</p>
-
-			
-		</aside>
-
-	</div>
+		</div>
+	</aside>
 
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 	<script src="js/menu-highlighter.js"></script>
 </body>
+
+
+<?php include "inc/footer.inc";?>
+
+        </div><!--.container -->
+        
+        <?php include "inc/scripts.inc";?>
+    </body>
+</html>
